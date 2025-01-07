44b19ca1dd048ceeba9b50a884922d313ffd56e4
Mike Newberry <hello@mikenew.xyz>
2025-01-07 23:15:47 +0000

    adjust env for tina

--- tina/config.ts
+++ tina/config.ts
@@ -2,18 +2,15 @@ import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
+  process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || "main";
 
 export default defineConfig({
   branch,
 
   // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  clientId: process.env.CLIENT_ID,
   // Get this from tina.io
-  token: process.env.TINA_TOKEN,
+  token: process.env.READ_TOKEN,
 
   build: {
     outputFolder: "admin",
