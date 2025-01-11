cda4bb44d078264a470743d6ac3b5589c76556a5
Mike Newberry <hello@mikenew.xyz>
2025-01-11 21:44:40 +0000

    add access scripts

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,9 +1,14 @@
 import type { CollectionConfig } from "payload";
+import { authenticated } from "@/access/authenticated";
+import { anyone } from "@/access/anyone";
 
 export const Media: CollectionConfig = {
 	slug: "media",
 	access: {
-		read: () => true,
+		create: authenticated,
+		delete: authenticated,
+		read: anyone,
+		update: authenticated,
 	},
 	fields: [
 		{
