c95573a62a238b79a2c870bd820a4f21b18951f2
Mike Newberry <hello@mikenew.xyz>
2025-01-26 22:04:24 +0000

    feat: add tailwind and lucide to payload routes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
 		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/collections/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {},
 	plugins: [],
